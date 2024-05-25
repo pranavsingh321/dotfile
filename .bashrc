f2515135a1772aefa89d99e665d49edd697cf4b4
Pranav Singh <pranav.singh@tsystems.com>
2024-05-25 10:24:06 +0000

    draft
    remove profile
    
    remove zprofile
    
    remove .profile
    
    some updte
    
    yabai padding
    
    add helix
    
    add dir
    
    update some
    
    update
    
    some helix
    
    some improvements
    
    left statusline

---
+++ .bashrc
@@ -0,0 +1,40 @@
+export HISTFILE=~/.bash_history
+export HISTSIZE=1000
+
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+alias ga="git add"
+alias gc="git commit"
+alias gf="git fetch"
+alias gl="git log"
+alias gb="git branch"
+alias gs="git status"
+alias gcm="git checkout master"
+alias gcd="git checkout develop"
+
+alias gd='git diff'
+alias gcb='git checkout -b'
+alias gc='git checkout'
+alias gcd='git checkout develop'
+alias gcp='git checkout preview'
+alias gca='git commit --amend '
+alias gcom='git commit -m'
+alias push='git push'
+alias pull='git pull'
+alias g='git'
+alias rgf='rg --files'
+alias j='jobs'
+alias v='nvim'
+alias mod='rg --files --hidden --stats | xargs -d "\n" stat --format="%Y %n" 2>/dev/null | sort -n | tail -n 3 | cut -d" " -f2-'
+
+eval "$(zoxide init bash --cmd cd)"
+export PS1="$ "
+
+if [ -f ~/.secret ]; then
+  source ~/.secret
+fi
+
+PATH=$PATH:~/.cargo/bin/
+[ -f ~/.fzf.bash ] && source ~/.fzf.bash
+. "$HOME/.cargo/env"
---
+++ link_dotfiles.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Directory to copy dotfiles from (current Git directory)
+SOURCE_DIR=$(pwd)
+
+# Home directory
+TARGET_DIR=$HOME
+
+# Get the name of the script itself
+SCRIPT_NAME=$(basename "$0")
+
+# Expected directory for dotfile
+EXPECTED_DIR="$HOME/dotfile"
+
+# Add secret
+echo "Copying secret sample..."
+cp -n "$SOURCE_DIR/.secret_sample" "$HOME/.secret"
+echo "Secret sample copied successfully."
+
+echo "Copy the helix folder"
+cp -rf $SOURCE_DIR/.config/helix $HOME/.config/
+
+# Check if the current directory is ~/dotfiles
+if [[ "$SOURCE_DIR" != "$EXPECTED_DIR" ]]; then
+    echo "Error: This script must be run from the ~/dotfiles directory."
+    exit 1
+fi
+
+# Function to create symbolic links or copy directories for dotfiles
+link_or_copy_dotfiles() {
+    for item in "$SOURCE_DIR"/.*; do
+        # Exclude .git directory, .secret* files, and the script itself
+        if [[ $(basename "$item") != ".git" && ! $(basename "$item") =~ ^\.secret && \
+              $(basename "$item") != "$SCRIPT_NAME" && $(basename "$item") != "." && \
+              $(basename "$item") != ".." ]]; then
+            if [[ -d "$item" ]]; then
+                continue
+            else
+                # If it's a file, create a symbolic link
+                echo "Creating symbolic link: $item to $TARGET_DIR/$(basename "$item")"
+                ln -sv "$item" "$TARGET_DIR/$(basename "$item")"
+            fi
+        fi
+    done
+}
+
+# Execute the function
+echo "Setting up dotfiles..."
+link_or_copy_dotfiles
+echo "Dotfiles setup complete."
